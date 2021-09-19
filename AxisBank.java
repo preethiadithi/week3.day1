56b7b6f6470a4f317a6999bf3c0806662a7ef0db
preethiadithi <90133101+preethiadithi@users.noreply.github.com>
2021-09-19 20:14:09 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,22 @@
+package week3day1assign;
+
+public class AxisBank extends BankInfo{
+	 public void deposit() {
+		// TODO Auto-generated method stub
+		 System.out.println("axisbak:deposits");
+
+	}
+	 public static void main(String[] args) {
+		 
+		 AxisBank ab = new AxisBank();
+		 ab.deposit();
+		 ab.savings();
+		 ab.fixed();
+		 
+		 
+		
+
+	}
+
+	}
+
---
+++ BankInfo.java
@@ -0,0 +1,22 @@
+package week3day1assign;
+
+public class BankInfo {
+
+	public void savings() {
+		// TODO Auto-generated method stub
+		System.out.println("bankinfo:savings");
+
+	}
+
+	public void fixed() {
+		// TODO Auto-generated method stub
+		System.out.println("bankinfo:fixed");
+	}
+
+	public void deposits() {
+		// TODO Auto-generated method stub
+		System.out.println("bankinfo:deposits");
+
+	}
+
+}
