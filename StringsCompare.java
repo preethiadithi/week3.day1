c725ed386f63e8de9ecca6340d50616b2bf46ac2
preethiadithi <90133101+preethiadithi@users.noreply.github.com>
2021-09-19 21:23:57 +0530

    Add files via upload

---
+++ StringsCompare.java
@@ -0,0 +1,18 @@
+package org.strings;
+
+public class StringsCompare {
+	
+	public static void main(String[] args) {
+		
+		String String1 =" I am learning java";
+		String String2 =" I am learning java?";
+	 if (String1.equalsIgnoreCase(String2))	
+	 {
+		 System.out.println(" both the strings are same");
+	 }
+	 else {
+		 System.out.println("not the same strings!");
+	 }
+	}
+
+}
