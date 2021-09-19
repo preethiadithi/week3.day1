dfac26148a761f1d3e8fd7e294a65f3356d050d7
preethiadithi <90133101+preethiadithi@users.noreply.github.com>
2021-09-19 21:18:31 +0530

    Add files via upload

---
+++ CharacterIndexReplace.java
@@ -0,0 +1,11 @@
+package org.strings;
+
+public class CharacterIndexReplace {
+	   public static void main(String[] args) {
+		     String text = "java exercise";
+		     System.out.println("the index of E is :" + text.indexOf('E'));
+		     System.out.println("the index of E is :" + text.indexOf('S'));
+		     
+		     }
+
+}
---
+++ StringSamples.java
@@ -0,0 +1,18 @@
+package org.strings;
+
+public class StringSamples {
+	
+	public static void main(String[] args) {
+		 String str3 =new String("kutty");
+		 String str4 =new String("kutty");
+		 if(str3==str4) {
+			 System.out.println("same text");
+		 
+	}
+		 else {
+			 System.out.println("diff text");
+		 }
+  System.out.println("***********");
+  
+	}
+}
